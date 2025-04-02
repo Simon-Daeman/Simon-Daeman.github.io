a83a8c1d484614baa1529cba965f112a7ea500b5
Simon-Daeman <Girraffesrule3000bc@gmail.com>
2025-04-02 14:17:10 -0400

    Add files via upload

---
+++ projects/Javascript/ch1/js01/project04/project01-04.js
@@ -0,0 +1,25 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Simon Still
+    Date: 3/16/2025
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+let homeStreet = "1 Main St";
+let workStreet = "15 Oak Ln";
+
+let homeCity = "Sicilia";
+let workCity = "Central City";
+
+let homeState = "MA";
+let workState = "MA";
+
+let homeCode = "02103";
+let workCode = "02104";
+
+
+
